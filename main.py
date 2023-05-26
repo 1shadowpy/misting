c9906969688c442418f6346cc346bc585ca1ea9b
1shadowpy <132641739+1shadowpy@users.noreply.github.com>
2023-05-26 11:02:06 +0500

    first commit

---
+++ main.py
@@ -0,0 +1,33 @@
+country = {
+    "Italya":"Rim",
+    "O'zbekiston":"Toshkent",
+    "AQSH":"Washington DC",
+    "Tojikiston":"Dushanbe",
+    "Rossia":"Moskva"
+}
+
+cars = {
+    "model":"BWM",
+    "color":"Yellow",
+    "price":"200000",
+}
+
+
+family = {
+    "father":"Anvarjon",
+    "mather":"Xafiza",
+    "brother":"Nurmuhmmad",
+    "sister": "Sarvinoz"
+}
+lugatlar = [country,cars,family]
+
+
+
+
+oilam = f"otamni ismi {lugatlar[2]['father']} \n onamni ismi {lugatlar[2]['mather']} \n ukamnining ismi {lugatlar[2]['brother']} \n opamni ismi {lugatlar[2]['sister']} \n "
+print(oilam)
+
+
+
+
+
